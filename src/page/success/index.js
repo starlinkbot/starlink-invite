8b27b42ba390dc59b0e2b9d5832b8d1b0a4f42f4
wangxiaohan <18718305995@163.com>
2024-01-04 19:49:02 +0800

    first commit2

--- src/page/success/index.js
+++ src/page/success/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { withRouter } from "react-router-dom"
 import { Button } from "antd-mobile"
 import { IMAGE } from "@/assets"
@@ -10,22 +10,24 @@ import "./styles.less"
 const IndexPage = ({ history }) => {
   const { t } = useTranslation()
   const { requestNoToken } = useRequest()
-  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    const getData = async () => {
-      const resp = await requestNoToken("app/home/app_check_update")
-      setData(resp)
+  const download = async () => {
+    setLoading(true)
+    const resp = await requestNoToken("app/home/app_check_update")
+    setLoading(false)
+    if (!resp?.error) {
+      window.open(resp?.download)
     }
-    getData()
-  }, [])
+  }
+
   
   return (
     <div className="p_success">
       <img src={IMAGE.logo} alt="" className="p_success_logo" />
       <p className="p_success_title">{ t("page.success.t1") }</p>
       <p className="p_success_subtitle">{ t("page.success.t2") }</p>
-      <Button className="p_success_button" onClick={() => window.open(data?.download)}>{ t("page.success.t3") }</Button>
+      <Button loading={loading} className="p_success_button" onClick={() => download()}>{ t("page.success.t3") }</Button>
       <img src={IMAGE.base} alt="" className="p_success_image" />
     </div>
   )
